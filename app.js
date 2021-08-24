f44f8c88b43bb9959cd1667d3ded3715faab28ca
Skyafrica <contact@skyafricasoft>
2021-08-24 16:30:56 +0100

    alora

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ const express = require('express')
 
 const app = express()
 
+const port = 3000
+
+//routes 
+app.get('/hello',(req,res)=>{
+    res.send('Task')
+})
+
 console.log('Task manager App')
+
+app.listen(port,console.log(`server listening on port 3000`))
